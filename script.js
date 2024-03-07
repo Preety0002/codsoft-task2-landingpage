ab6ea62757cdf63596039df77a228c99f912f964
Preety0002 <prityarju0002@gmail.com>
2024-03-07 11:55:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+gsap.from("#right img",{
+    // x:-500,
+    duration:3,
+    delay:1,
+    // repeat:-1,
+    yoyo:true,
+    scrub:2,
+    stagger:0.3,
+    opacity:0,
+    scale:0
+})
+gsap.from("#whole",{
+    // x:-500,
+    duration:3,
+    delay:1,
+    // repeat:-1,
+    yoyo:true,
+    scrub:2,
+    stagger:0.3,
+    opacity:0,
+    scale:0
+})
+// gsap.from(" .left1",{
+//     x:-500,
+//    // delay:1,
+//    duration:1,
+   
+//    scrollTrigger:{
+//        trigger:".left1",
+//        scroller:"body",
+//        // markers:true,
+//         start:"top 100%",
+//        end:"top 40%",
+//        scrub:1
+
+//        }
+// })
+// gsap.from(" .right1",{
+//     x:500,
+//    // delay:1,
+//    duration:1,
+   
+//    scrollTrigger:{
+//        trigger:".right1",
+//        scroller:"body",
+//        // markers:true,
+//         start:"top 100%",
+//        end:"top 40%",
+//        scrub:1
+
+//        }
+// })
+// gsap.from(" .shyam",{
+//     y:50,
+//     // opacity:0,
+//     scale:0,
+//    // delay:1,
+//    duration:1,
+   
+//    scrollTrigger:{
+//        trigger:".shyam",
+//        scroller:"body",
+//        // markers:true,
+//         start:"top 100%",
+//        end:"top 40%",
+//        scrub:1
+
+//        }
+// })
